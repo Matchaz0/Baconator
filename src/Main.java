1840b2db3471afe7958e808b4948dea7e047ebf5
Ryan Liu <Ryanliu12620@gmail.co>
2025-02-26 10:07:06 -0500

    I accidently commited to the wrong repo yesterday T-T

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        ArrayList<SimpleMovie> movies = MovieDatabaseBuilder.getMovieDB("MovieData/movie_data");
+        for (SimpleMovie movie : movies) {
+            System.out.println(movie);
         }
-        System.out.println("Test Commit");
+
+
+
+        SimpleMovie movie1 = movies.get(0);
+        System.out.println();
+        System.out.println(movie1.getTitle());
+        System.out.println(movie1.getActors());
+        System.out.println();
+        System.out.println("Number of movies: " + movies.size());
+
+        ArrayList<SimpleMovie> bacon = new ArrayList<SimpleMovie>();
+        ArrayList<String> actorsBacon1 = new ArrayList<String>();
+        for (SimpleMovie movie : movies) {
+            if (movie.getActors().contains("Kevin Bacon")) {
+                bacon.add(movie);
+            }
+        }
+        System.out.println(bacon.size());
     }
 }
